a54c6b041e93a23ca9b2d62561c2ac799807e0c6
Rakesh M <rakeshraki0180@gmail.com>
2023-07-25 06:00:36 +0530

    feat: mongodb connection WIP

--- app.js
+++ app.js
@@ -15,43 +15,6 @@ app.get("/", (req, res) => {
   res.send("welcome to home" + port);
 });
 
-app.get("/customers", (req, res) => {
-  res.json(customers);
-});
-
-app.get("/customers/:cid", (req, res) => {
-  const id = +req.params.cid;
-  const customer = customers.find((customer) => customer.id === id);
-  console.log(customer);
-  if (customer) {
-    res.json(customer);
-  } else {
-    {
-      res.json({ error: "Customer not found." });
-    }
-  }
-});
-
-app.post("/customers", (req, res) => {
-  const body = req.body;
-  console.log(body);
-  res.json(body);
-});
-
-//put
-app.put("/customers/:id", (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-  res.send(`put request sent to the server ${id}`);
-});
-
-// delete
-app.delete("/customers/:id", (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-  res.send(`delete request sent to the server ${id}`);
-});
-
 app.listen(port, () => {
   console.log("server running on port", port);
 });
